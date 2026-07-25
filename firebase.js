4c73610d80e670d609cab9373f982fb07e0aa687
pixelnovaai <asali20042005@gmail.com>
2026-07-25 10:51:14 +0500

    create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClUl486Em2Cq4PjOtal-3B-Gt_I5NqPCY",
+  authDomain: "nexacart-94526.firebaseapp.com",
+  projectId: "nexacart-94526",
+  storageBucket: "nexacart-94526.firebasestorage.app",
+  messagingSenderId: "843284374047",
+  appId: "1:843284374047:web:b8e18e6ed7b5326641eb2a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
