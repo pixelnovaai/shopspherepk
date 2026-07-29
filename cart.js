df789dbaba1e5fb8d476ce7194285d540b8dc313
pixelnovaai <asali20042005@gmail.com>
2026-07-29 14:22:49 +0500

    Fix cart system and remove button

--- cart.js
+++ cart.js
@@ -9,57 +9,56 @@ cartItems.innerHTML = "";
 
 cart.forEach((product, index) => {
 
-const price = Number(
-  product.price.toString().replace(/[^0-9]/g, "")
-);
+    const price = Number(
+        product.price.toString().replace(/[^0-9]/g, "")
+    );
 
-Rs. ${price}
+    total += price;
 
-cartItems.innerHTML += `
-<tr>
+    cartItems.innerHTML += `
+    <tr>
 
-<td>
-<img src="${product.image}" width="60"><br>
-${product.name}
-</td>
+        <td>
+            <img src="${product.image}" width="60"><br>
+            ${product.name}
+        </td>
 
-<td>
-Rs. ${price}
-</td>
+        <td>
+            Rs. ${price}
+        </td>
 
-<td>
-<button class="btn btn-danger btn-sm remove-btn" data-index="${index}">
-🗑 Remove
-</button>
-</td>
+        <td>
+            1
+        </td>
 
-</tr>
+        <td>
+            Rs. ${price}
+        </td>
 
-1
-</td>
+        <td>
+            <button class="btn btn-danger btn-sm remove-btn" data-index="${index}">
+                🗑 Remove
+            </button>
+        </td>
 
-<td>
-Rs. ${price}
-</td>
-
-</tr>
-`;
-
+    </tr>
+    `;
 });
 
 cartTotal.innerText = "Rs. " + total;
+
 document.addEventListener("click", function(e){
 
-if(e.target.classList.contains("remove-btn")){
+    if(e.target.classList.contains("remove-btn")){
 
-const index = e.target.dataset.index;
+        const index = e.target.dataset.index;
 
-cart.splice(index,1);
+        cart.splice(index,1);
 
-localStorage.setItem("cart",JSON.stringify(cart));
+        localStorage.setItem("cart", JSON.stringify(cart));
 
-location.reload();
+        location.reload();
 
-}
+    }
 
 });
